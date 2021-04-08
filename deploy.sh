6cfc712fa6e7e117bb5c8480154c86da60f71d1a
Abhinav Sharma <sharma.abhinav503@gmail.com>
2021-04-07 19:28:16 -0700

    Adding auto pr feature using person github token.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# This shell file deploys a new version to our server.
+# This shell file deploys a new version of staging branch to our server.
 
 export project_name=IndraABM
+
+echo "Sending a PR to master"
+python lib/github_utils.py
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $pa_pwd ssh -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
---
+++ lib/github_utils.py
@@ -0,0 +1,63 @@
+from github import Github, Repository
+import os
+
+TOKEN = 'GITHUB_TOKEN'
+INDRA_REPO = 'INDRA_REPO'
+token = os.getenv(TOKEN, None)
+
+BASE_BRANCH = 'master'
+
+HEAD_BRANCH = 'staging'
+
+ORGANIZATION = 'TandonDevOps'
+
+REPO_NAME = os.getenv(INDRA_REPO, 'IndraABM')
+
+"""
+Since I am using my own github account, the PR's are authored by me.
+Due to this, I cannot add myself as a reviewer automatically.
+Solution: Create a GitHub app/bot to create the PR.
+"""
+REVIEWERS = ['Denisfench', 'gcallah', 'NathanConroy']
+
+
+def pr_already_exists(repo: Repository.Repository):
+    pull_requests = repo.get_pulls(base=BASE_BRANCH, head=HEAD_BRANCH)
+    if pull_requests.totalCount != 0:
+        return True, pull_requests
+    else:
+        return False, None
+
+
+def generate_pr():
+    if not token:
+        raise EnvironmentError(
+            "Could not find github token. It is required to make an API "
+            "call to create a PR. "
+            "Please set it in your environment and re-start the build.")
+
+    hub = Github(token)
+    org = hub.get_organization(login=ORGANIZATION)
+    repo = org.get_repo(name=REPO_NAME)
+    # Do not try to create a new PR if there is a PR out already.
+    pr_exists, pull_requests = pr_already_exists(repo)
+    if not pr_exists:
+        head_branch = repo.get_branch(HEAD_BRANCH)
+        base_branch = repo.get_branch(BASE_BRANCH)
+        head_commit = head_branch.commit
+        body = head_commit.commit.message
+        title = f'Auto PR due to push to staging'
+        pr = repo.create_pull(base=base_branch.name, head=head_branch.name,
+                              body=body,
+                              maintainer_can_modify=True, title=title,)
+
+        pr.create_review_request(reviewers=REVIEWERS, team_reviewers=[])
+        print(f'Created PR: {pr.html_url}')
+    else:
+        print('PR already exists. The push should have updated the PR.')
+        for pr in pull_requests:
+            print(pr.html_url)
+
+
+if __name__ == "__main__":
+    generate_pr()
