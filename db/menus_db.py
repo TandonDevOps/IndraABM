23e85df28f3e86154401323c6cb78a066d6c9411
gcallah <gcallah@mac.com>
2021-04-23 22:25:55 -0400

    Abstracted out common menu laoding func.

--- db/menus_db.py
+++ db/menus_db.py
@@ -14,25 +14,23 @@ DEBUG_MENU_PATH = "/" + DB_DIR + "/" + DEBUG_MENU
 DEF_INDRA_HOME = utl.get_indra_home()
 
 
+def get_menu(menu_file):
+    try:
+        with open(menu_file) as file:
+            return json.loads(file.read())
+    except FileNotFoundError:
+        return None
+
+
 def get_debug_menu(indra_dir=DEF_INDRA_HOME):
     """
     Return the debug menu.
     """
-    debug_file = indra_dir + DEBUG_MENU_PATH
-    try:
-        with open(debug_file) as file:
-            return json.loads(file.read())
-    except FileNotFoundError:
-        return None
+    return get_menu(indra_dir + DEBUG_MENU_PATH)
 
 
 def get_model_menu(indra_dir=DEF_INDRA_HOME):
     """
     Return the model menu.
     """
-    mm_file = indra_dir + MODEL_MENU_PATH
-    try:
-        with open(mm_file) as file:
-            return json.loads(file.read())
-    except FileNotFoundError:
-        return None
+    return get_menu(indra_dir + MODEL_MENU_PATH)
