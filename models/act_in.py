eeb8902409cd03d860930d2e43d8cdfbc361de33
gcallah <gcallah@mac.com>
2021-07-24 10:56:46 -0400

    Developing activate-inihibition model.

--- models/act_in.py
+++ models/act_in.py
@@ -3,13 +3,8 @@ This model tries to demonstrate how patterns forms in a closed system
 """
 
 import lib.actions as acts
-from lib.agent import Agent
+import lib.model as mdl
 from lib.display_methods import RED, BLUE
-from lib.model import Model, NUM_MBRS, MBR_ACTION, NUM_MBRS_PROP, COLOR
-from lib.utils import Debug
-from registry.registry import save_reg
-
-DEBUG = Debug()
 
 MODEL_NAME = "act_in"
 DEF_INACTIVE_MBRS = 2
@@ -21,7 +16,7 @@ def act_in_action(agent, **kwargs):
     We're going to use this agent action to test the new get_neighbors()
     func in space.py.
     """
-    if DEBUG.debug:
+    if acts.DEBUG.debug:
         print("Agent {} is located at {}".format(agent.name,
                                                  agent.get_pos()))
     for neighbor in acts.get_neighbors(agent):
@@ -29,31 +24,23 @@ def act_in_action(agent, **kwargs):
     return acts.DONT_MOVE
 
 
-def create_agent(name, i, exec_key=None, action=act_in_action):
-    """
-    Create a agent
-    """
-    return Agent(name + str(i),
-                 action=action, exec_key=exec_key)
-
-
 act_in_grps = {
     "active": {
-        MBR_ACTION: act_in_action,
-        NUM_MBRS: DEF_ACTIVE_MBRS,
-        NUM_MBRS_PROP: "num_active",
-        COLOR: BLUE
+        mdl.MBR_ACTION: act_in_action,
+        mdl.NUM_MBRS: DEF_ACTIVE_MBRS,
+        mdl.NUM_MBRS_PROP: "num_active",
+        mdl.COLOR: BLUE
     },
     "inactive": {
-        MBR_ACTION: act_in_action,
-        NUM_MBRS: DEF_INACTIVE_MBRS,
-        NUM_MBRS_PROP: "num_inactive",
-        COLOR: RED
+        mdl.MBR_ACTION: act_in_action,
+        mdl.NUM_MBRS: DEF_INACTIVE_MBRS,
+        mdl.NUM_MBRS_PROP: "num_inactive",
+        mdl.COLOR: RED
     },
 }
 
 
-class ActIn(Model):
+class ActIn(mdl.Model):
     """
     Activation-inhibition model.
     """
