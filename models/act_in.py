3c6977e2bbb1d0d6aaf8821e6a272173be265fdc
Amadou <aod259@nyu.edu>
2021-07-29 10:29:42 -0400

    implemented cell action with 2 helper functions(getpower,calcuation inhibition value)

--- models/act_in.py
+++ models/act_in.py
@@ -5,13 +5,19 @@ This model tries to demonstrate how patterns forms in a closed system
 import lib.actions as acts
 import lib.model as mdl
 from lib.display_methods import RED, BLUE
+import registry.registry as reg
 
 MODEL_NAME = "act_in"
 DEF_INACTIVE_MBRS = 2
 DEF_ACTIVE_MBRS = 2
+DEF_ACT_STRENGTH = 1
+DEF_IN_STRENGTH = -0.1
+DEF_NEARBY_CELLS = 5
+DEF_FARTHER_CELLS = 10
+DEF_BIAS = 0
 
-DEF_NEARBY_CELLS = 2
-DEF_FARTHER_CELLS = 4
+ACTIVE = "active"
+INACTIVE = "inactive"
 
 
 def get_near_and_far_grps(agent):
@@ -21,30 +27,59 @@ def get_near_and_far_grps(agent):
     return (near_grp, far_grp)
 
 
+def group_power(grp, exec_key):
+    print(exec_key)
+    power = 0
+    for cell in grp:
+        cell = reg.get_agent(cell, exec_key)
+        if cell.group_name() == ACTIVE:
+            power += 1
+        else:
+            power -= 1
+    return power
+
+
+def act_val(act_power, in_power):
+    val = (act_power * DEF_ACT_STRENGTH
+           + in_power * DEF_IN_STRENGTH + DEF_BIAS)
+    return val
+
+
 def act_in_action(agent, **kwargs):
     """
     We're going to use this agent action to test the new get_neighbors()
     func in space.py.
     """
-    if acts.DEBUG.debug:
-        print("Agent {} is located at {}".format(agent.name,
-                                                 agent.get_pos()))
     (near_grp, far_grp) = get_near_and_far_grps(agent)
-    for neighbor in near_grp:
-        print(f"{str(agent)} has near neighbor {str(neighbor)}")
-    for neighbor in far_grp:
-        print(f"{str(agent)} has far neighbor {str(neighbor)}")
+    if acts.DEBUG.debug:
+        print("Agent {} is located at {}".format(agent.name, agent.get_pos()))
+        for neighbor in near_grp:
+            print(f"{str(agent)} has near neighbor {str(neighbor)}")
+        for neighbor in far_grp:
+            print(f"{str(agent)} has far neighbor {str(neighbor)}")
+    exec_key = agent.exec_key
+    act_power = group_power(near_grp, exec_key)
+    in_power = group_power(far_grp, exec_key)
+    act_in_val = act_val(act_power, in_power)
+    if act_in_val > 0:
+        if agent.group_name() != ACTIVE:
+            agent.has_acted = True
+            acts.add_switch(agent, INACTIVE, ACTIVE)
+    elif act_in_val < 0:
+        if agent.group_name() != INACTIVE:
+            agent.has_acted = True
+            acts.add_switch(agent, ACTIVE, INACTIVE)
     return acts.DONT_MOVE
 
 
 act_in_grps = {
-    "active": {
+    ACTIVE: {
         mdl.MBR_ACTION: act_in_action,
         mdl.NUM_MBRS: DEF_ACTIVE_MBRS,
         mdl.NUM_MBRS_PROP: "num_active",
         mdl.COLOR: BLUE
     },
-    "inactive": {
+    INACTIVE: {
         mdl.MBR_ACTION: act_in_action,
         mdl.NUM_MBRS: DEF_INACTIVE_MBRS,
         mdl.NUM_MBRS_PROP: "num_inactive",
