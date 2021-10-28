6da0d6f163a378ddc42abf274cb798cc82a037ab
Ralph Smith <ralph@ralphsmithphoto.com>
2021-10-28 13:04:09 -0500

    Moved import locations for Env in registry.py and get_agent in space.py

--- lib/space.py
+++ lib/space.py
@@ -9,6 +9,7 @@ from random import randint
 from lib.agent import is_group, AgentEncoder, X, Y
 from lib.group import Group
 from lib.utils import Debug
+from registry.registry import get_agent
 
 DEBUG = Debug()
 
@@ -410,9 +411,7 @@ class Space(Group):
         Return agent at cell x,y
         If cell is empty return None.
         Always make location a str for serialization.
-        Importing inside this function is to avoid a circular import
         """
-        from registry.registry import get_agent
         if self.is_empty(x, y):
             return None
         agent_nm = self.locations[str((x, y))]
