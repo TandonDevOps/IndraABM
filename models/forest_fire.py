d2e1cac549f571131144460683b9736042c1ffa6
Kieran Bissessar <kb2784@nyu.edu>
2021-11-21 19:09:09 -0500

    revert y-wind logic

--- models/forest_fire.py
+++ models/forest_fire.py
@@ -154,6 +154,7 @@ def wind_tree_action(agent, **kwargs):
 
 
 def y_wind_action(agent, **kwargs):
+    '''
     old_group = agent.group_name()
     new_group = old_group
     if old_group == HEALTHY:
@@ -178,7 +179,9 @@ def y_wind_action(agent, **kwargs):
         if acts.DEBUG.debug:
             print(f"Add switch from {old_group} to {new_group}")
         acts.add_switch(agent, old_group=old_group, new_group=new_group)
-    return acts.DONT_MOVE
+        '''
+    pass
+    #return acts.DONT_MOVE
 
 ff_grps = {
     HEALTHY: {
