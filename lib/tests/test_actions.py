87282805da9e28ee92054e44cc4a8c6eb218fdf5
zl233 <zilong2333@hotmail.com>
2021-11-28 17:59:54 -0500

    Add test for join in lib/actions.py

--- lib/tests/test_actions.py
+++ lib/tests/test_actions.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 
 import lib.actions as acts
 import lib.agent as agt
+import lib.group as grp
 
 TEST_AGENT = "test agent"
 
@@ -14,6 +15,9 @@ class ActionsTestCase(TestCase):
     def setUp(self):
         # We will just fake an exec key for this agent:
         self.agent = agt.Agent("Test agent", exec_key=None)
+        self.agent_for_group = agt.Agent("Test agent for group",
+                                          exec_key=None)
+        self.group = grp.Group("Test group")
 
     def tearDown(self):
         self.agent = None
@@ -34,3 +38,10 @@ class ActionsTestCase(TestCase):
         Test for default agent.
         """
         self.assertTrue(acts.def_action(self.agent))
+
+    def test_join(self):
+        """
+        Test join.
+        """
+        self.assertFalse(self.agent, self.group)
+        self.assertTrue(self.group, self.agent)
