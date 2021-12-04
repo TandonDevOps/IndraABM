f8bc318c3b8180a4eb9dfe5b7f0fdd1f1019def9
Jerry Lee <darkcb109xx@gmail.com>
2021-12-04 15:41:37 -0500

    added new setup.sh to not require gnu make

--- setup.sh
+++ setup.sh
@@ -1,13 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 # sets up a new developer
+export login_script=".bashrc"
 if [ -z $1 ]
 then
-    echo "Must pass name of login script (for instance .bash_profile)"
+    echo "Must pass the name of the project env variable."
+    exit 1
+fi
+export proj_home=$1
+if [ "$OSTYPE" == "linux-gnu" ]
+then
+    echo "In linux branch."
+    export login_script=".bashrc"
+elif [ "$OSTYPE" == "darwin" ]
+then
+    export login_script=".bash_profile"
+else
+    echo "Can't handle your OS; sorry!"
     exit 1
 fi
 
-export wd=`pwd`
-echo "export INDRA_HOME=$wd" >> $HOME/$1
-export INDRA_HOME=$wd
-echo "export PYTHONPATH=$wd:$PYTHONPATH" >> $HOME/$1
-export PYTHONPATH=$wd:$PYTHONPATH
+export demo_home_present=`grep $proj_home "$HOME/$login_script"`
+if [ -z "$demo_home_present" ]
+then
+    export wd=`pwd`
+    echo "export $proj_home=$wd" >> $HOME/$login_script
+    export $proj_home=$wd
+    echo "export PYTHONPATH=$wd:$PYTHONPATH" >> $HOME/$login_script
+    export PYTHONPATH=$wd:$PYTHONPATH
+else
+    echo "Detected $proj_home already in your login script."
+fi
