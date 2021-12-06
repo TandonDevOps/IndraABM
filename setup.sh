1d9c17c823abd55f4a01f702e6c7e1711d1e568d
Jerry Lee <darkcb109xx@gmail.com>
2021-12-05 19:44:40 -0500

    added windows detection to setup.sh

--- setup.sh
+++ setup.sh
@@ -14,19 +14,24 @@ then
 elif [ "$OSTYPE" == "darwin" ]
 then
     export login_script=".bash_profile"
+
+elif [ "$OSTYPE" == "msys" ]
+then
+    export login_script=".bash_profile"
+    echo "detected Windows (ignore the errors for now, login script is still made)"
 else
     echo "Can't handle your OS; sorry!"
     exit 1
 fi
 
-export demo_home_present=`grep $proj_home "$HOME/$login_script"`
+export demo_home_present=`grep $proj_home "$HOME/$login_script"` #detects if given value (IndraABM from make file) is in the login script (.bashrc or .bash_profile)
 if [ -z "$demo_home_present" ]
 then
     export wd=`pwd`
-    echo "export $proj_home=$wd" >> $HOME/$login_script
-    export $proj_home=$wd
-    echo "export PYTHONPATH=$wd:$PYTHONPATH" >> $HOME/$login_script
-    export PYTHONPATH=$wd:$PYTHONPATH
+    echo "export $proj_home=$wd" >> $HOME/$login_script 
+    export $proj_home=$wd # problem with windows on this line
+    echo "export PYTHONPATH=$wd:$PYTHONPATH" >> $HOME/$login_script 
+    export PYTHONPATH=$wd:$PYTHONPATH # problem with windows on this line
 else
     echo "Detected $proj_home already in your login script."
 fi
