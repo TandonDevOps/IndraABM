194fefc4b0cacd0de2f0b18d1f6e215256deaee9
m-dsouza <dsouzamichelle5@gmail.com>
2021-12-06 18:16:28 +0000

    Team 1 Resolving error in test file for minesweeper

--- models/tests/test_minesweeper.py
+++ models/tests/test_minesweeper.py
@@ -9,19 +9,19 @@ from models.minesweeper import Minesweeper, main, MODEL_NAME, minesweep_grps
 
 class MinesweeperTestCase(TestCase):
     def setUp(self):
-        self.mine = Minesweeper(MODEL_NAME, grp_struct=minesweep_grps)
+        self.mine = Minesweeper(MODEL_NAME, grp_struct=minesweep_grps,random_placing=False)
 
     def tearDown(self):
         self.mine = None
 
-    @skip("Problem at present with grid being all full.")
+    # @skip("Problem at present with grid being all full.")
     def test_run(self):
         """
         Does running the model work? (return of 0)
         """
         self.assertEqual(0, self.mine.run())
 
-    @skip("Problem at present with grid being all full.")
+    # @skip("Problem at present with grid being all full.")
     def test_main(self):
         """
         Does the main func of the model work? (return of 0)
