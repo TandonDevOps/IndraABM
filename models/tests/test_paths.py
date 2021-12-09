5ae56325a9aa5442d9171898a96404648fefc78c
zhanghang <hz2447@nyu.edu>
2021-12-08 19:24:22 -0500

    add setup for test_paths

--- models/tests/test_paths.py
+++ models/tests/test_paths.py
@@ -5,3 +5,10 @@ This is the test for the paths model.
 
 from unittest import TestCase
 from models.paths import Paths, main, MODEL_NAME
+from models.paths import paths_grps
+
+
+class PathsTestCase(TestCase):
+    def setUp(self):
+        self.paths = Paths(MODEL_NAME, grp_struct=paths_grps)
+
