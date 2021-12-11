0bd74d4e63f7edf2c1230d97896c7f6c0fb7e939
sheng wang <shw173@ucsd.edu>
2021-12-11 18:37:30 -0500

    Add testing file and case for model

---
+++ models/tests/test_party.py
@@ -0,0 +1,26 @@
+"""
+This is the tests for the party model
+"""
+
+from unittest import TestCase
+from models.party import MODEL_NAME, Party, party_grps
+from models.party import create_female, create_male
+
+
+class PartyTestCase(TestCase):
+    def setUp(self):
+        self.party = Party(MODEL_NAME, grp_struct=party_grps)
+        self.male = create_male("male", 0)
+        self.female = create_female("female", 0)
+
+    def tearDown(self):
+        self.party = None
+        self.male = None
+        self.female = None
+    
+    def test_run(self):
+        """
+        Does running the mode work? (return of 0)
+        """
+        self.assertEqual(0, self.party.run())
+    
