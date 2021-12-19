b892e28cbee956bd707a632a7e13ef9c4747163c
bojianwangny <bw2197@nyu.edu>
2021-12-19 18:46:00 +0000

    adding comments for test

--- models/tests/test_party.py
+++ models/tests/test_party.py
@@ -9,6 +9,9 @@ from models.party import create_female, create_male
 
 class PartyTestCase(TestCase):
     def setUp(self):
+        """
+        Initializing agents
+        """
         self.party = Party(MODEL_NAME, grp_struct=party_grps)
         self.male = create_male("male", 0)
         self.female = create_female("female", 0)
@@ -25,5 +28,8 @@ class PartyTestCase(TestCase):
         self.assertEqual(0, self.party.run())
     
     def test_main(self):
+        """
+        Does running the main method work? (return of 0)
+        """
         self.assertEqual(0, main())
     
